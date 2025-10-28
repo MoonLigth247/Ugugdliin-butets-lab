90818265397eda6a8a1deb1cece3fdb0e14bd283
MoonLigth247 <firesfires330@gmail.com>
2025-10-28 08:28:31 +0800

    Update Subject.java

--- BiyDaalt1/Code/Subject.java
+++ BiyDaalt1/Code/Subject.java
@@ -1,30 +1,14 @@
-// Subject.java
-/**
- * Subject класс: Хичээлийн мэдээллийг хадгалах класс
- * Хичээлийн код, нэр, кредит зэрэг мэдээллийг хадгална
- */
 public class Subject {
-    public String code;    // Хичээлийн код (жишээ нь: CS101)
-    public String name;    // Хичээлийн нэр (жишээ нь: Computer Programming)
-    public float credit;   // Хичээлийн кредит (жишээ нь: 3.0)
-    
-    /**
-     * Subject классын конструктор
-     * @param code Хичээлийн код
-     * @param name Хичээлийн нэр
-     * @param credit Хичээлийн кредит
-     */
+    public String code;  
+    public String name;   
+    public float credit; 
+
     public Subject(String code, String name, float credit) {
         this.code = code;
         this.name = name;
         this.credit = credit;
     }
-    
-    /**
-     * Хичээлийн мэдээллийг хэвлэх метод
-     * @return Хичээлийн мэдээлэл
-     */
-    @Override
+  
     public String toString() {
         return code + " - " + name + " (" + credit + " credit)";
     }
